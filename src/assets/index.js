6d2ec996928f6b04048cc1674a84d1b0c2c299d9
ElhamFadel <elham2000Fadel@gmail.com>
2021-09-22 15:45:55 +0300

    add list function

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,2 @@
 export { default as burn } from './burn.svg';
+export { default as arrow } from './arrow.svg';
--- src/ui/EmployeeRow/index.js
+++ src/ui/EmployeeRow/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { H5,H4 } from '../Typography'
+import { arrow } from '../../assets'
 import "./style.css";
 function EmployeeRow({key, employee:{first,last,created,balance} }) {
     return (
         <li className="employee-row" id={key}>
             {/* avater empolyee , date , name , balance , tools */}
             <div className="employee-row-avatar">
-                 <img src={`https://robohash.org/{${first } || elham}.png`} alt="avater" /> 
+                 {/* <img src={`https://robohash.org/{${first } || elham}.png`} alt="avater" />  */}
             </div>
             <H5 className="employee-row-date">
                 {created}
@@ -18,7 +19,15 @@ function EmployeeRow({key, employee:{first,last,created,balance} }) {
                 {balance}
             </H4>
             <div className="employee-row-tools">
-                
+                <img src={arrow} alt="setting" />
+                <ul className="employee-row-tools-list">
+                    <li className="drop-employee">
+                        Drop Empolyee
+                    </li>
+                    <li className="increase-balance">
+                        Increase Balance
+                    </li>
+                </ul>
             </div>
         </li>
     )
